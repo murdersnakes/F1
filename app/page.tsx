ff7b1662bfb3c1ab5996704e1503669d5dcf2c8e
murdersnakes <92579054+murdersnakes@users.noreply.github.com>
2023-07-26 16:15:40 +0100

    2

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import NextRace from './components/NextRace'
-import DriversChampionship from './components/DriversChampionship'
-import ConstructorsChampionship from './components/ConstructorsChampionship'
+import React from "react";
+import NextRace from "./components/NextRace";
+import DriversChampionship from "./components/DriversChampionship";
+import ConstructorsChampionship from "./components/ConstructorsChampionship";
 
 export default function page() {
   return (
-    <div className='max-w-7xl mx-auto'>
-        <NextRace />
-        <div className='flex justify-center mt-10 gap-10'>
-            <DriversChampionship />
-            <ConstructorsChampionship />
-        </div>
+    <div className="max-w-7xl mx-auto">
+      <NextRace />
+      <div className="flex flex-col lg:flex-row justify-center mt-10 gap-10">
+        <DriversChampionship />
+        <ConstructorsChampionship />
+      </div>
     </div>
-  )
+  );
 }
